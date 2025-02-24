8fcf9945ffd1862d0feb1ad067336b35a329f0c6
aliceyayachen <aliceyayachen@gmail.com>
2025-02-24 17:27:56 +0000

    asdfad

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,1 +1,21 @@
 package org.example; //DO NOT DELETE
+import java.util.Scanner;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the number of gallons: ");
+        int gallons = scan.nextInt();
+
+        System.out.println("In " + gallons + " gallons there are: ");
+        System.out.println("" + gallons*4.0 + " quarts");
+        System.out.println("" + gallons*8.0 + " pints");
+        System.out.println("" + gallons*16.0 + " cups");
+        System.out.println("" + gallons *256.0 + " tablespoons");
+    }
+
+
+
+}
